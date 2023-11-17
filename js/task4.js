4549b9eac68f2c6398960fce52fcb8e856c91338
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-11-18 00:35:55 +0200

    fix-html

--- js/task4.js
+++ js/task4.js
@@ -46,49 +46,97 @@
 // console.log(getTotalBalanceByGender(allUsers, 'female')); // 8863
 
 
-const gettotalbalancebygender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-};
+// const gettotalbalancebygender = (users, gender) => {
+//   return users
+//     .filter(user => user.gender === gender)
+//     .reduce((total, user) => total + user.balance, 0);
+// };
 
-const allusers = [
-  {
-    name: "moore hensley",
+// const allusers = [
+//   {
+//     name: "moore hensley",
+//     gender: "male",
+//     balance: 2811
+//   },
+//   {
+//     name: "sharlene bush",
+//     gender: "female",
+//     balance: 3821
+//   },
+//   {
+//     name: "ross vazquez",
+//     gender: "male",
+//     balance: 3793
+//   },
+//   {
+//     name: "elma head",
+//     gender: "female",
+//     balance: 2278
+//   },
+//   {
+//     name: "carey barr",
+//     gender: "male",
+//     balance: 3951
+//   },
+//   {
+//     name: "blackburn dotson",
+//     gender: "male",
+//     balance: 1498
+//   },
+//   {
+//     name: "sheree anthony",
+//     gender: "female",
+//     balance: 2764
+//   }
+// ];
+
+// console.log(gettotalbalancebygender(allusers, "male")); // 12053
+// console.log(gettotalbalancebygender(allusers, "female")); // 8863
+
+const getTotalBalanceByGender = (users, gender) => {
+    return users.filter(user => user.gender === gender)
+                .reduce((acc, user) => acc + user.balance,  0);
+                
+}
+
+const allUsers = [
+    {
+    name: "Moore Hensley",
     gender: "male",
     balance: 2811
   },
   {
-    name: "sharlene bush",
+    name: "Sharlene Bush",
     gender: "female",
     balance: 3821
   },
   {
-    name: "ross vazquez",
+    name: "Ross Vazquez",
     gender: "male",
     balance: 3793
   },
   {
-    name: "elma head",
+    name: "Elma Head",
     gender: "female",
     balance: 2278
   },
   {
-    name: "carey barr",
+    name: "Carey Barr",
     gender: "male",
     balance: 3951
   },
   {
-    name: "blackburn dotson",
+    name: "Blackburn Dotson",
     gender: "male",
     balance: 1498
   },
   {
-    name: "sheree anthony",
+    name: "Sheree Anthony",
     gender: "female",
     balance: 2764
   }
 ];
 
-console.log(gettotalbalancebygender(allusers, "male")); // 12053
-console.log(gettotalbalancebygender(allusers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
